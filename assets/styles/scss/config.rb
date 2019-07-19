9e4cdbf17fb3985e867a2a0d740050147b15642f
Ben Kaminski <ben@benkaminski.com>
2019-07-19 14:55:34 -0400

    set up sass and Gulp

---
+++ assets/styles/scss/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "../styles"
+sass_dir = "./"
+images_dir = "../img"
+javascripts_dir = "../js"
+environment = :production
+relative_assets = true
+output_style = :compressed
+preferred_syntax = :scss
