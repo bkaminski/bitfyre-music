61b8fa19e5e051fb88f70cde9fb325460c3e4a6d
Ben Kaminski <ben@benkaminski.com>
2020-04-16 11:43:55 -0400

    editing scss

--- functions/theme-dependencies.php
+++ functions/theme-dependencies.php
@@ -10,7 +10,7 @@ add_action('wp_enqueue_scripts', 'enqueue_bitfyre_scripts');
 //LOAD CSS
 function enqueue_bitfyre_styles() {
     wp_enqueue_style('bootstrap-4x', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
-    wp_enqueue_style('dmi-styles', get_template_directory_uri() . '/assets/styles/bitStyles.min.css');
+    wp_enqueue_style('bitfyre-styles', get_template_directory_uri() . '/assets/styles/bitStyles.min.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_bitfyre_styles');
 
